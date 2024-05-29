617fc59e8d03a515cab77eb7a9da9bf3b8a20e6d
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2024-05-29 16:22:22 -0400

    add abstract class

--- IntroToTs/classes.ts
+++ IntroToTs/classes.ts
@@ -7,15 +7,71 @@ export class Person {
     age:number;
     gender:Gender;
 
-    // static variables
+    // static variables, is the class variable, can be called without generate instance
     static nation = "China";
+    //
+    readonly height = 168;
 
     constructor(name:string,age:number,gender:Gender) {
         this.name = name;
         this.age = age;
         this.gender = gender;
     }
+
+    // define a function within class
+    sayHello(){
+       console.log("Hello from",this.name);
+    }
+
+    intro(){
+        console.log(this);
+    }
 }
+
+class Dog extends Animal{
+    name: string;
+    age: number;
+
+    constructor(name:string,age:number) {
+        super();
+        this.age=age;
+        this.name = name;
+    }
+
+    bark(){
+        console.log("dog bark");
+    }
+
+    makeNoise() {
+    }
+}
+
+export class Shiba extends Dog{
+
+    color:Color;
+
+    constructor(name:string,age:number,color:Color) {
+        super(name,age);
+        this.color = color;
+    }
+
+    makeNoise() {
+        // super.bark();
+        console.log("shiba bark");
+    }
+}
+
+export enum Color {
+    WHITE,
+    BLACK,
+    RED
+}
+
+abstract class Animal {
+    abstract makeNoise();
+}
+
+
 
 export let personOne = new Person("nina",18,Gender.Female);
 
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
-import {Person} from "../IntroToTs/classes";
+import {Color, Person, Shiba} from "../IntroToTs/classes";
 import {Gender} from "../IntroToTs/types";
 
 console.log("this is the file to be packed using web pack");
 
 let personOne = new Person("nina", 18, Gender.Female);
+let shibaOne = new Shiba("Gee",3,Color.RED)
 
 console.log("person name is",personOne.name)
+console.log(personOne.height);
+console.log(Person.nation);
+console.log(personOne.sayHello());
+console.log(personOne.intro());
