c9c0f1b5759da38b873ffeca462ad745508f8643
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2024-06-04 16:20:03 -0400

    add generics

--- IntroToTs/classes.ts
+++ IntroToTs/classes.ts
@@ -12,33 +12,38 @@ export class Person {
     //
     readonly height = 168;
 
-    constructor(name:string,age:number,gender:Gender) {
+    constructor(name: string, age: number, gender: Gender) {
         this.name = name;
         this.age = age;
         this.gender = gender;
     }
 
     // define a function within class
-    sayHello(){
-       console.log("Hello from",this.name);
+    sayHello():String {
+        console.log("Hello from", this.name);
+        return "Hello from "+this.name;
     }
 
-    intro(){
+    intro() {
         console.log(this);
     }
 }
 
-class Dog extends Animal{
+abstract class Animal {
+    abstract makeNoise() :void;
+}
+
+class Dog extends Animal {
     name: string;
     age: number;
 
-    constructor(name:string,age:number) {
+    constructor(name: string, age: number) {
         super();
-        this.age=age;
+        this.age = age;
         this.name = name;
     }
 
-    bark(){
+    bark() {
         console.log("dog bark");
     }
 
@@ -46,19 +51,22 @@ class Dog extends Animal{
     }
 }
 
-export class Shiba extends Dog{
+export class Shiba extends Dog implements Pet {
 
-    color:Color;
+    color: Color;
 
-    constructor(name:string,age:number,color:Color) {
-        super(name,age);
+    constructor(name: string, age: number, color: Color, owner: Person) {
+        super(name, age);
         this.color = color;
+        this.owner = owner;
     }
 
     makeNoise() {
         // super.bark();
         console.log("shiba bark");
     }
+
+    owner: Person;
 }
 
 export enum Color {
@@ -67,12 +75,13 @@ export enum Color {
     RED
 }
 
-abstract class Animal {
-    abstract makeNoise();
+
+
+interface Pet {
+    owner: Person;
 }
 
 
-
-export let personOne = new Person("nina",18,Gender.Female);
+export let personOne = new Person("nina", 18, Gender.Female);
 
 
--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import {Gender} from "../IntroToTs/types";
 
 console.log("this is the file to be packed using web pack");
 
-let personOne = new Person("nina", 18, Gender.Female);
-let shibaOne = new Shiba("Gee",3,Color.RED)
+let personOne = new Person("Nina", 18, Gender.Female);
+let shibaOne = new Shiba("A Ji",3,Color.RED,personOne)
 
-console.log("person name is",personOne.name)
+console.log("person name is",personOne.age)
 console.log(personOne.height);
 console.log(Person.nation);
 console.log(personOne.sayHello());
 console.log(personOne.intro());
+console.log(shibaOne.name,"'s owner is",shibaOne.owner.sayHello());
