39c60fb5e3e979eed0edc30305672f5f7874bd7b
Sean-Xiao-Liu <glxiaoliu@gmail.com>
2024-06-17 21:07:12 -0400

    add basic put and delete requests

--- cypress/e2e/custom tests/basic-api-tests.cy.ts
+++ cypress/e2e/custom tests/basic-api-tests.cy.ts
@@ -1,8 +1,9 @@
 import {Pages} from "@support/pages";
+import {response} from "express";
 
 describe('Basic API tests', () => {
 
-    context('CRUD',() => {
+    context('basic CRUD',() => {
         it('basic GET request',() => { // set browser to run tests
             cy.request('GET',
                 Pages.getJsonPlaceHolderUrl()+'posts/1')
@@ -49,6 +50,29 @@ describe('Basic API tests', () => {
                         id: 101
                     });
                     expect(response.body.id).eql(101);
+                })
+        })
+
+        it('basic PUT request',() => {
+            cy.request('PUT',Pages.getJsonPlaceHolderUrl()+'posts/1',{
+                title:'post title',
+                body:'post body',
+                userId:1,
+                id:1
+            })
+                .then((response) =>{
+                    expect(response.status).eql(200);
+                    expect(response.body.userId).eql(1);
+                    expect(response.body.id).eql(1);
+                })
+        })
+
+        it('basic DELETE request',() => {
+            cy.request('DELETE',Pages.getJsonPlaceHolderUrl()+'posts/1')
+                .then((response) =>{
+                    expect(response.status).eql(200);
+                    // expect(response.body.userId).eql(1);
+                    // expect(response.body.id).eql(1);
                 })
         })
 
